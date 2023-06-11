fff688fa3ffdfc18d4bd3dca35e6176d5d795849
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-06-11 09:47:22 -0500

    Print stats

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::{BufWriter};
 use std::str;
 use my_seqio::reader::{DynamicFastXReader, FastXReader};
 use my_seqio::writer::DynamicFastXWriter;
-use core::cmp::min;
+use core::cmp::{min,max};
 
 mod cli;
 mod partial_suffix_sort;
@@ -37,27 +37,41 @@ fn main() {
 
     eprintln!("Running partial suffix sort");
     let partial_SA = partial_suffix_sort::partial_suffix_sort(&seq_concat, k);
-
-/*
     let n = partial_SA.len();
 
+    eprintln!("Inverting partial SA");
     let mut inverse_partial_SA = vec![0usize; n];
 
     for i in 0..n{
         inverse_partial_SA[partial_SA[i]] = i;
     }
 
+    eprintln!("Finding k-mer runs");
+    for _ in 0..k{
+        seq_concat.push(b'$'); // Padding so that k-mers can go over the end
+    }
     let mut kmer_run_start = 0 as usize;
+    let mut n_runs: usize = 0;
+    let mut max_run: usize = 0;
     for i in 0..n{
         let text_pos = partial_SA[i];
-        if i > 0{
+        if i > 0 {
+            // Do we have the start of a new run?
             let prev_text_pos = partial_SA[i-1];
             if seq_concat[text_pos..text_pos+k] != seq_concat[prev_text_pos..prev_text_pos+k]{
-                
+                n_runs += 1;
+                max_run = max(max_run, i - kmer_run_start);
+                kmer_run_start = i;
             }
         }
     }
-*/
+    // Last run
+    n_runs += 1;
+    max_run = max(max_run, n - kmer_run_start);
+
+    eprintln!("Found {} k-mer runs", n_runs);
+    eprintln!("Average run lengths: {}", n as f64 / n_runs as f64);
+    eprintln!("Longest run is {} k-mers", max_run);
 
 }
 
