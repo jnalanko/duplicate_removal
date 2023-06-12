e5b691d3e4403e91a661c9c858e058ac8f26fe9a
Jarno N. Alanko <alanko.jarno@gmail.com>
2023-06-12 15:41:00 -0500

    Length difference bound

--- src/main.rs
+++ src/main.rs
@@ -33,21 +33,30 @@ fn check_for_duplicates(doc_array: &Vec<usize>, cumul_seq_lengths: &Vec<usize>, 
         for j in i+1..SA_end{
             let id1 = doc_array[i];
             let id2 = doc_array[j];
+
+            let s1 = extract_sequence(id1, seqs_concat, cumul_seq_lengths);
+            let s2 = extract_sequence(id2, seqs_concat, cumul_seq_lengths);
+
             if id1 == id2{
                 continue; // Same read
             }
 
-            let s1 = extract_sequence(id1, seqs_concat, cumul_seq_lengths);
-            let s2 = extract_sequence(id2, seqs_concat, cumul_seq_lengths);
-            if !already_tested.contains(&(id1,id2)) {
-                //let result = edit_distance::edit_distance(str::from_utf8(s1).unwrap(), str::from_utf8(s2).unwrap());
-                let result = triple_accel::levenshtein(s1, s2);
-                already_tested.insert((id1,id2));
-                let d = result as f64; 
-                if d < max(s1.len(), s2.len()) as f64 * 0.05 { // Less than 5% edit distance compared to the length of the longer sequence 
-                    eprintln!("read {} (length {}) vs read {} (length {}): {}", id1, s1.len(), id2, s2.len(), result);
-                }
+            if usize::abs_diff(s1.len(), s2.len()) > 20{
+                continue; // Too different in length
             }
+
+            if already_tested.contains(&(id1,id2)) {
+                continue; // Already tested
+            }
+
+            //let result = edit_distance::edit_distance(str::from_utf8(s1).unwrap(), str::from_utf8(s2).unwrap());
+            let result = triple_accel::levenshtein(s1, s2);
+            already_tested.insert((id1,id2));
+            let d = result as f64;
+            if d < max(s1.len(), s2.len()) as f64 * 0.05 { // Less than 5% edit distance compared to the length of the longer sequence 
+                eprintln!("read {} (length {}) vs read {} (length {}): {}", id1, s1.len(), id2, s2.len(), result);
+            }
+            
         }
     }
 
